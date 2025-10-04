f9235fd0889654a924cf0388f86a31e40b4bcc84
zfdang <zfdang@sparsity.xyz>
2025-10-04 06:41:13 +0000

    chore: expose __version__ from package metadata

--- aws_nitro_enclaves/__init__.py
+++ aws_nitro_enclaves/__init__.py
@@ -1,5 +1,11 @@
 """AWS Nitro Enclaves Python SDK."""
 
 from . import nsm  # noqa: F401
+from importlib import metadata
 
-__all__ = ["nsm"]
+try:
+    __version__ = metadata.version("aws-nitro-enclaves-python-sdk")
+except metadata.PackageNotFoundError:  # pragma: no cover - local dev
+    __version__ = "0.0.0"
+
+__all__ = ["nsm", "__version__"]
